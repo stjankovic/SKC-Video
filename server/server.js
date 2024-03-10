13459805875c7560cd4a40de4e530dc157739f09
stjankovic <stefanjankovic31@gmail.com>
2024-03-10 15:23:43 +0100

    Fix

--- server/server.js
+++ server/server.js
@@ -1,36 +1,17 @@
 const express = require('express');
-const app = express();
-const { exec } = require('child_process');
+const path = require('path');
 
+const app = express();
 const PORT = 3000;
 
-// Middleware to parse JSON bodies. This must come before any routes that will need it.
 app.use(express.json());
+app.use(express.static('public')); // Serve static files from 'public' directory
 
 app.get('/', (req, res) => {
-  // The URL you want to open in Chromium
-  const url = 'http://skc.local:3000/';
-
-  // Command to open Chromium in fullscreen mode
-  // Adjust the path to Chromium as necessary for your system
-  const command = `chromium-browser --kiosk '${url}'`;
-
-  // Execute the command to open Chromium in fullscreen
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return res.send('Error opening Chromium');
-    }
-    res.send('Hello from Raspberry Pi - Chromium should now be open in fullscreen mode.');
-  });
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Serve your index.html file
 });
 
-app.post('/start', (req, res) => {
-  const data = req.body; // Access data from the body of the request
-  console.log('Received data from ESP8266:', data);
-  
-  res.status(200).send();
-});
+// Other routes...
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
